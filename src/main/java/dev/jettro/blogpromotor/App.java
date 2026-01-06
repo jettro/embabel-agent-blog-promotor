fd2840e4f1cfbaaadc7f2a9a02b8537f39970ded
Jettro Coenradie <jettro@coenradie.com>
2026-01-06 09:20:15 +0100

    More updates for 0.3.1 release

--- src/main/java/dev/jettro/blogpromotor/App.java
+++ src/main/java/dev/jettro/blogpromotor/App.java
@@ -1,8 +1,5 @@
 package dev.jettro.blogpromotor;
 
-import com.embabel.agent.config.annotation.EnableAgents;
-import com.embabel.agent.config.annotation.LoggingThemes;
-import com.embabel.agent.config.annotation.McpServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Main application class for the Agent Blog Promoter
  */
 @SpringBootApplication
-@EnableAgents(loggingTheme = LoggingThemes.STAR_WARS, mcpServers = McpServers.DOCKER)
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
